39d8362d0bcc00c6570b1662113a26d993390b2d
Администратор <@WIN-AH7O15KNHIQ>
2016-11-09 15:56:15 +0200

    my fix

--- src/java3/JAVA3.java
+++ src/java3/JAVA3.java
@@ -15,7 +15,13 @@ public class JAVA3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        System.out.println ("Дубей Елена Васильенва");
+        /*
+        фио
+        */
+     
+// TODO code application logic here
     }
     
 }
